ae6b20282801c9210d6c0aef178bd65271c300fb
kaiyz <kaiyz@vt.edu>
2018-06-26 01:27:04 -0400

    Update leveldataoverride.lua

--- DSTClusterConfig/Master/leveldataoverride.lua
+++ DSTClusterConfig/Master/leveldataoverride.lua
@@ -83,7 +83,7 @@ return {
     weather="default",
     wildfires="default",
     winter="default",
-    world_size="medium",
+    world_size="large",
     wormhole_prefab="wormhole" 
   },
   random_set_pieces={
